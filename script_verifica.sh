1df380b929a7513505a80a570b0b81d6ae72bb5e
Sarah Cabral <bysarahcabral@gmail.com>
2025-06-30 21:44:52 -0300

    script adicionado

---
+++ script_verifica.sh
@@ -0,0 +1,26 @@
+
+#!/bin/bash
+
+. /verifica_servidor/.env
+
+URL="192.168.1.16"
+
+requisicaoHTTP=$(curl -Is $URL | head -n 1)
+
+# Definição de cores para melhor visualização dos logs
+fonteVermelho='\033[0;31m'
+fonteVerde='\033[0;32m'
+fonteSemCor='\033[0m' # reset de cor
+
+formatacaoData=$(date "+%Y/%m/%d %H:%M:%S")
+
+if [ "$requisicaoHTTP" ]; then
+  printf "${fonteVerde}${formatacaoData} Servidor Online ($URL)${fonteSemCor}\n"
+else
+  printf "${fonteVermelho}${formatacaoData} Servidor Offline ($URL)${fonteSemCor}\n"
+  # Envio da mensagem para o Discord e ocultacao de saida do curl para o log
+  curl -H "Content-Type: application/json" \
+       -X POST \
+       -d "{\"content\": \"🔴 [$formatacaoData] - **Servidor Offline**\"}" \
+       "$discordWebhook" > /dev/null 2>&1
+fi
